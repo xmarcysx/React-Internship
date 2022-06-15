fbb904cc38f0088752fad4831e74513cd269daf4
xmarcysx <marcinchowaniec21@gmail.com>
2022-06-15 17:25:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const students = [
+  {
+    name: "Martin",
+    lastName: "Lant",
+    marks: [9, 8, null, 7, 5],
+  },
+  {
+    name: "Francesco",
+    lastName: "Portus",
+    marks: [5, 4, 2, 3, 2],
+  },
+  {
+    name: "Bill",
+    lastName: "Merdoc",
+    marks: [10, null, null, null, 10],
+  },
+  {
+    name: "John",
+    lastName: "Entman",
+    marks: [9, 8, 9, 7, 5],
+  },
+];
